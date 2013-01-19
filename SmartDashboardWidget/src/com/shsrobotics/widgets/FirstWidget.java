a5c9dd8dfd4fd92093577e8bd9ec43ada8ce485c
Max Orth <max.orth96@gmail.com>
2013-01-19 11:12:15 -0800

    ignore

--- SmartDashboardWidget/src/com/shsrobotics/widgets/FirstWidget.java
+++ SmartDashboardWidget/src/com/shsrobotics/widgets/FirstWidget.java
@@ -2,15 +2,19 @@ package com.shsrobotics.widgets;
 
 import edu.wpi.first.smartdashboard.gui.StaticWidget;
 import edu.wpi.first.smartdashboard.properties.Property;
+import javax.swing.JLabel;
 
 
 
 
 public class FirstWidget extends StaticWidget{
-
+    
+    JLabel label;
+    
     @Override
     public void init() {
-        
+        label = new JLabel("HELLO WORLD");
+        this.add(label);
     }
 
     @Override
