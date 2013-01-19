8ff7b8658e9b4736429681ac04a1ed002adee354
Max Orth <max.orth96@gmail.com>
2013-01-19 10:56:23 -0800

    added nb project with a widget class

---
+++ SmartDashboardWidget/src/com/shsrobotics/widgets/FirstWidget.java
@@ -0,0 +1,21 @@
+package com.shsrobotics.widgets;
+
+import edu.wpi.first.smartdashboard.gui.StaticWidget;
+import edu.wpi.first.smartdashboard.properties.Property;
+
+
+
+
+public class FirstWidget extends StaticWidget{
+
+    @Override
+    public void init() {
+        
+    }
+
+    @Override
+    public void propertyChanged(Property prprt) {
+        
+    }
+    
+}
